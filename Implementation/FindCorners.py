bcd29c7eaa80794a40c1ea533efc836b427d09f4
Tomáš Adam <tomas.adam@student.tuke.sk>
2019-12-14 20:48:32 +0100

    Repository structure cleanup & performance preparation

---
+++ Implementation/FindCorners.py
@@ -0,0 +1,95 @@
+import cv2
+import numpy as np
+import operator
+
+def readImage(filename):
+    img = cv2.imread(filename, 0)
+    if img is None:
+        print('Invalid image:' + filename)
+        return None
+    else:
+        print('Image successfully read...')
+        return img
+
+
+def findCorners(img, maxCor, thresh, dst):
+    # Pre-processing tests
+    img = imagePreProcessing(img)
+
+    # Find x and y derivatives
+    dy, dx = np.gradient(img)
+    Ixx = dx ** 2
+    Iyy = dy ** 2
+    # Ixy = dy*dx
+
+    height = img.shape[0]
+    width = img.shape[1]
+
+    window_size = (min(img.shape[0], img.shape[1]) / 1000) * 5
+    offset = int(window_size/25)
+
+    cornerList = []
+    # Search corners in image
+    for y in range(offset, height - offset):
+        for x in range(offset, width - offset):
+            # Calculate sum of squares
+            windowIxx = Ixx[y - offset:y + offset + 1, x - offset:x + offset + 1]
+            windowIyy = Iyy[y - offset:y + offset + 1, x - offset:x + offset + 1]
+            # windowIxy = Ixy[y-offset:y+offset+1, x-offset:x+offset+1]
+
+            Sxx = windowIxx.sum()
+            Syy = windowIyy.sum()
+            # Sxy = windowIxy.sum()
+
+            # Find determinant and trace, for Harris corner detection
+            # det = (Sxx * Syy) - (Sxy**2)
+            # trace = Sxx + Syy
+
+            # If lambda1 and lambda2 have large positive
+            # values, then a corner is found.
+            r = min(Sxx, Syy)
+            # r = det - k*(trace**2)
+
+            # Threshold for corner
+            if r > thresh:
+                cornerList.append([x, y, r])
+
+    return filterCornersByDistance(cornerList, dst)[0:maxCor]
+
+
+def filterCornersByDistance(cornerList, dist):
+    cornerList.sort(key=operator.itemgetter(2), reverse=True)  # by r
+    eFiltered = [cornerList[0]]
+    for x in cornerList:
+        bigger = True
+        for y in eFiltered:
+            # euclidean distance comparison
+            if ((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2) ** .5 <= dist:
+                bigger = False
+                break
+        if bigger:
+            eFiltered.append(x)
+    return eFiltered
+
+
+def imagePreProcessing(img):
+    # img = cv2.blur(img, (4, 4))
+    # img = cv2.GaussianBlur(img, (3, 3), 0)
+
+    # Laplace 2
+    # kernel = np.array([[1, 1, 1],
+    #                   [1, -8, 1],
+    #                   [1, 1, 1]])
+
+    # Laplace 1
+    # kernel = np.array([[0, 1, 0],
+    #                   [1, -4, 1],
+    #                   [0, 1, 0]])
+
+    # Conv matrix 3x3 1/9
+    # kernel = np.ones((3, 3), np.float32) / 9
+
+    # img = cv2.filter2D(img, -1, kernel)
+    # cv2.imwrite("testImage.png", img)
+    return img
+
---
+++ Implementation/Main.py
@@ -0,0 +1,74 @@
+import FindCorners as fc
+import numpy as np
+import cv2
+import time
+
+
+def assignmentTest(img, maxCor, thresh, dst):
+    outfile = open('../PerformanceExperiments/assignmentCorners.txt', 'w')
+
+    for i in range(1000):
+        t1 = time.time()
+        fc.findCorners(img, maxCor, thresh, dst)
+        t2 = time.time()
+        outfile.write(str(t2 - t1) + '\n')
+    outfile.close()
+
+
+def goodFeatureTest(img, maxCor, thresh, dst):
+    outfile = open('../PerformanceExperiments/builtInCorners.txt', 'w')
+    for i in range(1000):
+        t1 = time.time()
+        cv2.goodFeaturesToTrack(img, maxCor, thresh, dst)
+        t2 = time.time()
+        outfile.write(str(t2 - t1) + '\n')
+    outfile.close()
+
+
+def main():
+    img = fc.readImage('../images/combined.png')
+
+    if img is not None:
+        if len(img.shape) == 3:
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        if len(img.shape) == 4:
+            img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+
+        # Specifying maximum number of corners as maxCorners
+        # thresh is the minimum quality level below which the corners are rejected
+        # dist is the minimum euclidean distance between two corners
+        maxCorners = 500
+        thresh = 0.1
+        dist = 10
+
+        # Assignment Shi-Tomasi
+        assignmentTest(img, maxCorners, thresh, dist)
+
+        # Built-in Shi-Tomasi
+        goodFeatureTest(img, maxCorners, thresh, dist)
+
+        # Visualization
+        color_img = fc.readImage('../images/combined.png')
+        color_img = cv2.cvtColor(color_img, cv2.COLOR_GRAY2RGB)
+
+        # Assignment Shi-Tomasi
+        assignmentCorners = fc.findCorners(img, maxCorners, thresh, dist)
+
+        for corner in assignmentCorners:
+            cv2.circle(color_img, (corner[0], corner[1]), 3, 255, -1)
+            cv2.imwrite("../OtherExperiments/assignmentImage.png", color_img)
+
+        color_img = fc.readImage('../images/combined.png')
+        color_img = cv2.cvtColor(color_img, cv2.COLOR_GRAY2RGB)
+
+        # Built-in Shi-Tomasi
+        builtInCorners = np.int0(cv2.goodFeaturesToTrack(img, maxCorners, thresh, dist))
+
+        for corner in builtInCorners:
+            x, y = corner.ravel()
+            cv2.circle(color_img, (x, y), 3, 255, -1)
+            cv2.imwrite("../OtherExperiments/builtInImage.png", color_img)
+
+
+if __name__ == "__main__":
+    main()
